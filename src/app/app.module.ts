a46717adf3d7b6342a9338e7e0914e635b4c12f2
glegshot <sreekumar.y2k@gmail.com>
2021-05-14 14:28:48 +0530

    cleaned up changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JokeListComponent } from './joke-list/joke-list.component';
-import { ModelFormComponentComponent } from './model-form-component/model-form-component.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JokeListComponent,
-    ModelFormComponentComponent
-  ],
+    AppComponent  
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot(), 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
